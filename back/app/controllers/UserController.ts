dfb786c01a1cd6166c357a8bd8ff39ed956facb4
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2019-02-16 20:31:03 -0300

    login estudante

--- back/app/controllers/UserController.ts
+++ back/app/controllers/UserController.ts
@@ -1,14 +1,14 @@
 import User from "../models/User";
-import Student from "../models/Student";
+import Student, {IStudent} from "../models/Student";
 import Database from "../config/Database";
 import mongoose = require('mongoose');
-
+import Error from '../models/ErrorCode';
 class UserController {
 
     async registerUser(type : String, body :any) : Promise<boolean>{
 
         let result :boolean = await Database.connect();
-        if (!result) return false;
+        if (!result)  throw new Error(500,"Falha ao conectar-se com o banco de dados");
 
         switch (type){
             case "student":{
@@ -17,9 +17,38 @@ class UserController {
                 return result;
             }
             default:{
-                return false;
+                throw new Error(400,"Tipo de usuário inexistente");
             }
         }
+    }
+
+    async loginUser(type : String, body :any) : Promise<any>{
+
+        let result :boolean = await Database.connect();
+        if (!result)  throw new Error(500,"Falha ao conectar-se com o banco de dados");
+
+        switch (type){
+            case "student":{
+                let result = await this.loginStudent(body.email, body.password);
+                await Database.disconnect();
+                return result;
+            }
+            default:{
+                throw new Error(400,"Tipo de usuário inexistente");
+            }
+        }
+
+    }
+
+    private async loginStudent(email :string, password :string) : Promise<any> {
+        return new Promise<any>( async (response) =>{
+            Student.findOne({email: email, password: password}, (err, res) => {
+                if(res == null || err) throw new Error(401, "Usuário não encontrado");
+                else{
+                    response(res);
+                }
+            });
+        });
     }
 
     private registerStudent(body :any) : Promise<boolean> {
--- back/app/routes/UserRoute.ts
+++ back/app/routes/UserRoute.ts
@@ -2,32 +2,56 @@ import Route from "../models/Route";
 import authController from "../controllers/AuthController";
 import e = require("express");
 import UserController from '../controllers/UserController';
+import Error from '../models/ErrorCode';
 
 class UserRoute implements Route {
     applyRoute(app: e.Application): void {
 
         app.get('/user', (req, res) =>{
+            //TODO
             res.send(authController.generateToken());
         });
 
-        app.post('/user/login/:type', (req, res) =>{
+        app.post('/user/login/:type', async (req, res) =>{
+
+            // let token = req.headers.authorization;
+            // let isValid :boolean = await authController.isValidToken(token);
+
+            // if(isValid){
+                try{
+                    let result = await  UserController.loginUser(req.params.type, req.body);
+                    res.send(result);
+                }catch (e) {
+                    if(e instanceof Error){
+                        res.sendStatus(e.code);
+                    } else res.sendStatus(500);
+                }
+            // } else {
+            //     res.sendStatus(403);
+            // }
 
         });
 
         app.post('/user/register/:type', async (req, res) => {
+
             let token = req.headers.authorization;
             let isValid :boolean = await authController.isValidToken(token);
-            if(!isValid) res.sendStatus(403);
-            else{
-                UserController.registerUser(req.params.type, req.body).then((result) => {
-                   if(result) res.sendStatus(200);
-                   else res.sendStatus(500);
-                });
+
+            try{
+                if(!isValid) res.sendStatus(403);
+                else{
+                    await UserController.registerUser(req.params.type, req.body);
+                    res.sendStatus(200);
+                }
+            }catch (e) {
+                if(e instanceof Error){
+                    res.sendStatus(e.code);
+                } else res.sendStatus(500);
             }
         });
 
         app.post('user/delete/:userId', (req, res) =>{
-
+            //TODO
         });
     }
 }
